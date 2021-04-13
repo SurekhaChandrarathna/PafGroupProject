ffa265769dc5863fd938a6512a967cb73f3535b7
SurekhaChandrarathna <surekhamaduhansi@gmail.com>
2021-04-13 11:22:55 +0530

    Create DBConnection & DbUtil java files

---
+++ GadgetBadget/src/it19132938/DBConnection.java
@@ -0,0 +1,31 @@
+package it19132938;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+	
+	private static String url = "jdbc:mysql://localhost:3306/gadgetbudget";
+	private static String user = "root";
+	private static String pass = "";
+	private static Connection con;
+	
+
+	public static Connection getConnection() {
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			con = DriverManager.getConnection(url, user, pass);
+			System.out.println("Database connection is successfull!!!");
+			
+		}
+		catch (Exception e) {
+			System.out.println("Database connection is not success!!!");
+		}
+		
+		return con;
+	}
+
+}
---
+++ GadgetBadget/src/it19132938/DbUtill.java
@@ -0,0 +1,18 @@
+package it19132938;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+
+public class DbUtill {
+	
+	private static Connection con = null;
+	private static Statement stat = null;
+	private static ResultSet result = null;
+
+}
